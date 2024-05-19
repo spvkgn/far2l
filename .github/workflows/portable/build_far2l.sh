e385c5007eb1e303bf9010de5a7019d7d683fcb7
spvkgn <spvkgn@users.noreply.github.com>
2024-05-19 19:26:53 +0500

    test

--- .github/workflows/portable/build_far2l.sh
+++ .github/workflows/portable/build_far2l.sh
@@ -17,6 +17,7 @@ mkdir -p $BUILD_DIR && \
 cmake -S . -B$BUILD_DIR \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
   -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache \
   -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache \
   ${CMAKE_OPTS[@]} && \
@@ -26,7 +27,7 @@ if [[ "$STANDALONE" == "true" ]]; then
   ( cd $BUILD_DIR/install && ./far2l --help >/dev/null && bash -x $HERE_DIR/make_standalone.sh ) && \
   ( cd $REPO_DIR && makeself --keep-umask --nomd5 --nocrc $BUILD_DIR/install $PKG_NAME.run "FAR2L File Manager" ./far2l && \
     tar cvf ${PKG_NAME/_${VERSION}-${GH_NAME}}.run.tar $PKG_NAME.run )
-fi
+fi && \
 
 tar cJvf $REPO_DIR/far2l.tar.xz -C $REPO_DIR/AppDir .
 
